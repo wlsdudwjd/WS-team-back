4eefd94abcf38e877192a87960d548d960fd228f
wlsdudwjd <jjyabc11@naver.com>
2025-12-17 20:47:08 +0900

    init

---
+++ src/main/java/com/example/itsme/ItsmeApplication.java
@@ -0,0 +1,13 @@
+package com.example.itsme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItsmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/itsme/ItsmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itsme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
